372814a85e7a91f567a7c9c5b31688a3cc7ef21e
Thomas Wilkinson <thomas.wilkinson@us.ibm.com>
2020-10-30 22:21:11 -0500

    lint on tests

--- test/30-crashtest.js
+++ test/30-crashtest.js
@@ -6,41 +6,40 @@
  */
 
 const entropy = require('../lib/entropy');
-const expect = require('chai').expect;
 
 const longTextSample = require('fs').readFileSync(
-    __dirname + '/longtextsample.txt').toString();
+  __dirname + '/longtextsample.txt').toString();
 
 describe('Crash test', function() {
-    it('entropy of undefined', function() {
-        entropy();
-    });
-    it('entropy of a null password', function() {
-        entropy(null);
-    });
-    it('entropy of an empty password', function() {
-        entropy('');
-    });
-    it('entropy of a non-string', function() {
-        entropy({});
-    });
-    it('entropy of a simple password', function() {
-        entropy('password');
-    });
-    it('entropy with special characters', function() {
-        entropy('this:password');
-    });
-    it('entropy with a complex emoji', function() {
-        entropy('this👨‍👩‍👧‍👦password');
-    });
-    it('entropy with hanzi', function() {
-        entropy('this我password');
-    });
-    it('entropy with less-common hanzi', function() {
-        entropy('this魏password');
-    });
-    it('entropy of war and peace', function() {
-        entropy(longTextSample);
-    })
+  it('entropy of undefined', function() {
+    entropy();
+  });
+  it('entropy of a null password', function() {
+    entropy(null);
+  });
+  it('entropy of an empty password', function() {
+    entropy('');
+  });
+  it('entropy of a non-string', function() {
+    entropy({});
+  });
+  it('entropy of a simple password', function() {
+    entropy('password');
+  });
+  it('entropy with special characters', function() {
+    entropy('this:password');
+  });
+  it('entropy with a complex emoji', function() {
+    entropy('this👨‍👩‍👧‍👦password');
+  });
+  it('entropy with hanzi', function() {
+    entropy('this我password');
+  });
+  it('entropy with less-common hanzi', function() {
+    entropy('this魏password');
+  });
+  it('entropy of war and peace', function() {
+    entropy(longTextSample);
+  });
 });
 
