927c1575262c9fad4b53c3805d8dd28fd470732e
Guillaume Royer <guillaumeroyer.mail@gmail.com>
2021-10-25 10:00:22 +0200

    feat(typescript): add missing `config` and `addCommonPasswords` methods

--- lib/entropy.d.ts
+++ lib/entropy.d.ts
@@ -22,11 +22,44 @@ interface EntropyInfo {
   sets: TokenSet[];
   length: number;
   entropy: number;
+  max_entropy_scale?: number;
   acceptable: boolean;
   ideal: boolean;
   legal: boolean;
 }
 
-declare const entropy: (password: string) => EntropyInfo;
+interface EntropyConfig {
+  /**
+   * The lowest entropy score considered acceptable.
+   * That is, in the return object, the lowest entropy that have `acceptable == true`.
+   *
+   * Default: `64`.
+   */
+  minAcceptable: number;
+  /**
+   * The lowest entropy score considered ideal.
+   * If `minIdeal` is set lower that `minAcceptable`, it will be ignored and `ideal` will always be true when `acceptable` is true.
+   *
+   * Default: `96`.
+   */
+  minIdeal: number;
+  /**
+   * A string or an array of strings, each being either a token set name or an alias for a preset list of token set names.
+   * Currently supported aliases are `'all'` and `'western'`.
+   *
+   * Default: `'all'`.
+   */
+  sets: string|string[];
+}
+
+interface Entropy {
+  (password: string): EntropyInfo;
+  addCommonPasswords(passwords: string[]): EntropyInfo;
+  addCommonPasswords(...passwords: string[]): EntropyInfo;
+  config(config: Partial<EntropyConfig>): void;
+  config(key: keyof EntropyConfig, value?: number|string|string[]): void;
+}
+
+declare const entropy: Entropy;
 
 export default entropy;
